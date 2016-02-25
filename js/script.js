c1c504f4acfd0c652c996e5aea009f29dc79924b
jacobhorne <jacobhorne@gmail.com>
2016-02-24 22:55:32 -0500

    pb

--- js/script.js
+++ js/script.js
@@ -2,4 +2,30 @@
 
 (function( window, undefined ){
 
+	//random images on refresh.
+	$(window).load(function () {
+		var images = ['home.jpg', 'about.jpg', 'employee.jpg', 'retiree.jpg'];
+		$('#background').css({'background-image': 'url(img/' + images[Math.floor(Math.random() * images.length)] + ')'});
+	});
+
+	//Adding Active States to Navigation
+	$(function() {
+     	var url = window.location.href.substr(window.location.href.lastIndexOf("/")+1);
+    
+	    $(".full-navigation a").each(function(){
+	        if($(this).attr("href") == url || $(this).attr("href") == '' )
+	        $(this).attr('id', 'activeNav');
+	    })
+	});
+
+	//Adding Active States to Sidebar
+	$(function() {
+     	var url = window.location.href.substr(window.location.href.lastIndexOf("/")+1);
+    
+	    $(".sidebar ul a").each(function(){
+	        if($(this).attr("href") == url || $(this).attr("href") == '' )
+	        $(this).attr('id', 'activeSideBar');
+	    })
+	});
+
 })( window, undefined );
