abfa87ca85b02da651646ef65626cc8cf5a70320
jacobhorne <jacobhorne@gmail.com>
2016-02-29 11:09:39 -0500

    Final Changes

--- footer.php
+++ footer.php
@@ -1,28 +1,24 @@
-<?php
-    <footer>
-        <div class="newsletter">
-            <p>Sign Up for Our Newsletter</p>
-            <input type="email" name="email" placeholder="Email Address"> 
-            <button type="submit">SIGN UP</button>
+<footer>
+    <div class="newsletter">
+        <p>Sign Up for Our Newsletter</p>
+        <input type="email" name="email" placeholder="Email Address"> 
+        <button type="submit">SIGN UP</button>
+    </div>
+
+    <div class="social-footer">
+        <nav>
+            <ul>
+                <li><a href="index.html">HOME</a></li>
+                <li><a href="about.html">ABOUT</a></li>
+                <li><a href="index.html">SUPPORT</a></li>
+                <li><a href="index.html">CONTACT</a></li>
+            </ul>
+        </nav>
+        <div>
+            <a href=""><img src="img/twitter.svg" alt""></a>
+            <a href=""><img src="img/instagram.svg" alt""></a>
+            <a href=""><img src="img/facebook.svg" alt""></a>
         </div>
-
-        <div class="social-footer">
-            <nav>
-                <ul>
-                    <li><a href="index.html">HOME</a></li>
-                    <li><a href="about.html">ABOUT</a></li>
-                    <li><a href="index.html">SUPPORT</a></li>
-                    <li><a href="index.html">CONTACT</a></li>
-                </ul>
-            </nav>
-
-            <div>
-                <a href=""><img src="img/twitter.svg" alt""></a>
-                <a href=""><img src="img/instagram.svg" alt""></a>
-                <a href=""><img src="img/facebook.svg" alt""></a>
-            </div>
-
-            <p>Copyright 2016 Plan Benefit Services, Inc.</p>
-        </div>
-    </footer>
-?>
+        <p>Copyright 2016 Plan Benefit Services, Inc.</p>
+    </div>
+</footer>
